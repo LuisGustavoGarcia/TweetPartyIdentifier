4b22cd186812327007e10f9204a3d744a596f2e1
Luis Garcia <LuisGustavoGarcia@users.noreply.github.com>
2021-01-14 17:12:52 -0500

    Remove API Keys

--- CongressMemberAccountFinder.py
+++ CongressMemberAccountFinder.py
@@ -10,10 +10,10 @@ import pandas as pd
     './Generated Data/CongressMemberTwitterHandles.csv'
 '''
 if __name__ == "__main__":
-    api = twitter.Api(consumer_key='O1TcM1r6K0hKZ7bnpjslpxaG5',
-                  consumer_secret='4gSL38BmKvs63PU3lucl7e4Gz60sTWPnnoxQ3IBO2vEjFxJI9U',
-                  access_token_key='1331694691256070144-nks9sd74IiAyt8e4CCHktQwUNTDBor',
-                  access_token_secret='iCwbCbaHfBHpJ2fs7Hov0LCGBbAQov0nEiSVbs2bgXnUV')
+    api = twitter.Api(consumer_key='',
+                  consumer_secret='',
+                  access_token_key='',
+                  access_token_secret='')
 
     # https://twitter.com/TheDemocrats/lists/house-democrats/members
     house_democrats = api.GetListMembers('110250128')
